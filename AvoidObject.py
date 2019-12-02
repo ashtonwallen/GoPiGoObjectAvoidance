393c630f673bf3b340dc318fb9bfa2ea48d0f4ee
ashtonwallen <aashton482@gmail.com>
2019-12-02 15:32:27 -0500

    moved things

--- AvoidObject.py
+++ AvoidObject.py
@@ -1,14 +1,15 @@
 import time
 import easygopigo3 as easy
 
-gpg = easy.EasyGoPiGo3()
-dist_sensor = gpg.init_distance_sensor()
 TURN_DISTANCE = 20
 TURN_DEGREES = 25
 
+gpg = easy.EasyGoPiGo3()
+dist_sensor = gpg.init_distance_sensor()
+
 while True:
-    print("Distance From Object (mm): " + str(my_distance_sensor.read_mm()))
-    dist = my_distance_sensor.read_mm()
+    print("Distance From Object (mm): " + str(dist_sensor.read_mm()))
+    dist = dist_sensor.read_mm()
     if dist <= TURN_DISTANCE:
         gpg.turn_degrees(TURN_DEGREES)
     time.sleep(.1)
